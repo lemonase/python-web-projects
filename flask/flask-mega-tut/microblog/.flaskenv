ec7791960879da9268235a3bbc6b19ec2d18341e
James Dixon <notjamesdixon@gmail.com>
2020-04-27 18:45:41 -0400

    add ability to edit user profile

--- flask/flask-mega-tut/microblog/.flaskenv
+++ flask/flask-mega-tut/microblog/.flaskenv
@@ -1,2 +1,2 @@
 export FLASK_APP=microblog.py
-export FLASK_ENV=devlopment
+export FLASK_ENV=development
--- flask/flask-mega-tut/microblog/app/routes.py
+++ flask/flask-mega-tut/microblog/app/routes.py
@@ -1,10 +1,12 @@
+from datetime import datetime
+
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
 from app import app, db
-from app.models import User
-from app.forms import LoginForm, RegistrationForm
+from app.models import User, Post
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -72,13 +74,46 @@ def logout():
     return redirect(url_for("index"))
 
 
-@app.route("/")
-@app.route("/index")
-@login_required
-def index():
+@app.route("/user/<username>")
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
     posts = [
         {"author": {"username": "john"}, "body": "Beautiful day here in chicago!"},
         {"author": {"username": "billy"}, "body": "I like turtles"},
     ]
+    return render_template("user.html", user=user, posts=posts)
+
+@app.route("/edit_profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your changes have been saved")
+        return redirect(url_for("user", username=current_user.username))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template("edit_profile.html", title='Edit Profile', form=form)
+
+
+@app.route("/")
+@app.route("/index")
+@login_required
+def index():
+    # posts = [
+    #     {"author": {"username": "john"}, "body": "Beautiful day here in chicago!"},
+    #     {"author": {"username": "billy"}, "body": "I like turtles"},
+    # ]
+    posts = Post.query.all()
 
     return render_template("index.html", title="Home", posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()

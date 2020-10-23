e86def54f5416c0ccfcb590d0bb1769aad6be885
James Dixon <notjamesdixon@gmail.com>
2020-10-23 13:59:15 -0400

    add models for db

--- flask/flask-todo/todo/app.py
+++ flask/flask-todo/todo/app.py
@@ -1,8 +1,10 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost:5432/flask_todo'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
 db = SQLAlchemy(app)
 
 
